94e32da0233c7280b06bf271622de7051470bd4b
s2h25 <keithenvalentine@unomaha.edu>
2025-01-29 22:21:07 +0000

    End of the day 3

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,16 +10,16 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-answers = ["Do not bet on it", "yes","for sure", "maybe later", "no", 
+answers = ["Do not bet on it", "yes", "for sure", "maybe later", "no", 
  "Alexa says yes", "try again", "without a doubt", "nope"]
 
   #Answer question randomly with one of the options from your earlier list.
 length = len('answers') 
-r = random.random() * length
+r = random.random () * length
 r = int(r)
 print(r)
-response = 'answers' [r]
-print(response)
+response = answers[r]
+print(response) 
 
 
 if __name__ == '__main__':
